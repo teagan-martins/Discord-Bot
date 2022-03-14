a36aee217eb2e856358e7ee0f8109b62a93549b7
Teagan Martins <88857742+zip-per@users.noreply.github.com>
2022-03-13 23:06:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import discord
-from discord.ext import commands
 import random
-import requests
+from asyncio import sleep
 import bs4
+import discord
+import matplotlib.pyplot as plt
+import requests
 
 token_file = open('token.txt','r')
 token = token_file.readline()
@@ -18,7 +19,6 @@ for line in facts_list:
     count+=1
 
 bot = discord.Client()
-vbot = commands.Bot('$')
 
 @bot.event
 async def on_ready():
@@ -29,6 +29,7 @@ async def on_message(message):
 
     if message.content.lower() == "$jimmypic":
         num = random.randint(1,4)
+
         if num == 1:
             with open("Jimmy1.JPG", 'rb') as f:
                 picture = discord.File(f)
@@ -70,7 +71,6 @@ async def on_message(message):
 
     if message.content.lower()[0:7] == '$avatar':
         user_avatar = await bot.fetch_user(int(message.content[7:]))
-        print(user_avatar)
         await message.channel.send(user_avatar.avatar_url_as(format=None, static_format='webp', size=1024))
 
     if message.content.lower() == '$fact':
@@ -94,15 +94,134 @@ async def on_message(message):
         await message.channel.send(shortened_element[quote_list[0]+1:quote_list[1]])
 
 
-    if message.author.id == USER ID HERE:
-        await message.channel.send('YOUR MESSAGE HERE')
+    if message.author.id == 371022437883707392:
+        chance = random.randint(1,50)
+        if chance == 14:
+            await message.channel.send('Hello Nathan')
 
-    if message.author.id == USER ID HERE:
-        await message.channel.send('YOUR MESSAGE HERE')
+    if message.author.id == 271426173781671937:
+        chance2 = random.randint(1,50)
+        if chance2 == 23:
+            await message.channel.send('Hello Jimmy')
 
+    if message.content.lower()[0:5] == '$spam' and message.author.id == 467042361881395200:
+        slash_list = [slash for slash in range(len(message.content.lower())) if message.content.lower().startswith("/",slash)]
+
+        num_spam = int(message.content[slash_list[0]+1:slash_list[1]])
+
+        await message.delete()
+
+        for x in range(num_spam):
+            await message.channel.send(message.content[slash_list[1]+1:])
+
+    if message.content.lower()[0:5] == '$poll':
+        slash_list = [slash for slash in range(len(message.content.lower())) if message.content.lower().startswith("/",slash)]
+
+        option_count = len(slash_list)-1
+
+        option_list = []
+        for x in slash_list:
+            option_list.append(x)
+
+        option_list.pop(0)
+
+        if option_count==1:
+           bot_message = await message.channel.send(message.content[slash_list[0]+1:slash_list[1]] + '\n' + ':red_circle: = ' + message.content[slash_list[1]+1:])
+           await bot_message.add_reaction('🔴')
+
+           await sleep(10)
+           bot_msg = discord.utils.get(bot.cached_messages, id=bot_message.id)
+           rcreaction = discord.utils.get(bot_msg.reactions, emoji='🔴')
+
+           plt.bar([message.content[slash_list[1]+1:]],[rcreaction.count-1], color = ['salmon'])
+
+           plt.xlabel('Options')
+           plt.ylabel('Count')
+           plt.title(message.content[slash_list[0]+1:slash_list[1]])
+           plt.savefig('bar.png')
+           plt.clf()
+
+           with open("bar.png", 'rb') as f:
+               picture = discord.File(f)
+               await message.channel.send(file = picture)
+
+        elif option_count==2:
+            bot_message = await message.channel.send(message.content[slash_list[0]+1:slash_list[1]] + '\n' + ':red_circle: = ' + message.content[slash_list[1]+1:slash_list[2]] + '\n' + ':blue_circle: = ' + message.content[slash_list[2]+1:])
+            await bot_message.add_reaction('🔴')
+            await bot_message.add_reaction('🔵')
+
+            await sleep(10)
+
+            bot_msg = discord.utils.get(bot.cached_messages, id=bot_message.id)
+            rcreaction = discord.utils.get(bot_msg.reactions, emoji='🔴')
+            bcreaction = discord.utils.get(bot_msg.reactions, emoji='🔵')
+
+            plt.bar([message.content[slash_list[1]+1:slash_list[2]], message.content[slash_list[2]+1:]],[rcreaction.count-1,bcreaction.count-1], color = ['salmon','cornflowerblue'])
+
+            plt.xlabel('Options')
+            plt.ylabel('Count')
+            plt.title(message.content[slash_list[0]+1:slash_list[1]])
+            plt.savefig('bar.png')
+            plt.clf()
+
+            with open("bar.png", 'rb') as f:
+                picture = discord.File(f)
+                await message.channel.send(file = picture)
+
+        elif option_count==3:
+           bot_message =  await message.channel.send(message.content[slash_list[0]+1:slash_list[1]] + '\n' + ':red_circle: = ' + message.content[slash_list[1]+1:slash_list[2]] + '\n' + ':blue_circle: = ' + message.content[slash_list[2]+1:slash_list[3]] + '\n' + ':yellow_circle: = ' + message.content[slash_list[3]+1:])
+           await bot_message.add_reaction('🔴')
+           await bot_message.add_reaction('🔵')
+           await bot_message.add_reaction('🟡')
+
+           await sleep(10)
+
+           bot_msg = discord.utils.get(bot.cached_messages, id=bot_message.id)
+           rcreaction = discord.utils.get(bot_msg.reactions, emoji='🔴')
+           bcreaction = discord.utils.get(bot_msg.reactions, emoji='🔵')
+           ycreaction = discord.utils.get(bot_msg.reactions, emoji='🟡')
+
+           plt.bar([message.content[slash_list[1]+1:slash_list[2]], message.content[slash_list[2]+1:slash_list[3]], message.content[slash_list[3]+1:]],[rcreaction.count-1,bcreaction.count-1,ycreaction.count-1], color = ['salmon','cornflowerblue','gold'])
+
+           plt.xlabel('Options')
+           plt.ylabel('Count')
+           plt.title(message.content[slash_list[0]+1:slash_list[1]])
+           plt.savefig('bar.png')
+           plt.clf()
+
+           with open("bar.png", 'rb') as f:
+               picture = discord.File(f)
+               await message.channel.send(file = picture)
+
+        elif option_count==4:
+            bot_message = await message.channel.send(message.content[slash_list[0]+1:slash_list[1]] + '\n' + ':red_circle: = ' + message.content[slash_list[1]+1:slash_list[2]] + '\n' + ':blue_circle: = ' + message.content[slash_list[2]+1:slash_list[3]] + '\n' + ':yellow_circle: = ' + message.content[slash_list[3]+1:slash_list[4]] + '\n' + ':green_circle: = ' + message.content[slash_list[4]+1:])
+            await bot_message.add_reaction('🔴')
+            await bot_message.add_reaction('🔵')
+            await bot_message.add_reaction('🟡')
+            await bot_message.add_reaction('🟢')
+
+            await sleep(10)
+
+            bot_msg = discord.utils.get(bot.cached_messages, id=bot_message.id)
+
+            rcreaction = discord.utils.get(bot_msg.reactions, emoji='🔴')
+            bcreaction = discord.utils.get(bot_msg.reactions, emoji='🔵')
+            ycreaction = discord.utils.get(bot_msg.reactions, emoji='🟡')
+            gcreaction = discord.utils.get(bot_msg.reactions, emoji='🟢')
+
+
+            plt.bar([message.content[slash_list[1]+1:slash_list[2]], message.content[slash_list[2]+1:slash_list[3]], message.content[slash_list[3]+1:slash_list[4]], message.content[slash_list[4]+1:]],[rcreaction.count-1,bcreaction.count-1,ycreaction.count-1,gcreaction.count-1], color = ['salmon','cornflowerblue','gold','limegreen'])
+            plt.xlabel('Options')
+            plt.ylabel('Count')
+            plt.title(message.content[slash_list[0]+1:slash_list[1]])
+            plt.savefig('bar.png')
+            plt.clf()
+
+            with open("bar.png", 'rb') as f:
+                picture = discord.File(f)
+                await message.channel.send(file = picture)
 
     if message.content.lower() == "$commands":
-        await message.channel.send("Commands:\n$JimmyPic - Send a random picture of Jimmy\n$Coin - Flip a coin\n$Dice - Roll a die\n$Avatar(userID) - Send an enlarged picture of someone's avatar\n$Fact - Send a random fun fact\n*Commands are not case-sensitive*")
-
+        await message.channel.send("Commands:\n$jimmypic - Send a random picture of Jimmy\n$coin - Flip a coin\n$dice - Roll a die\n$avatar(userID) - Send an enlarged picture of someone's avatar\n$fact - Send a random fun fact\n$search(word/string) - Send a random image off Yahoo Images related to that specified search term\n$poll/question/option/option/option/option - Sends a poll for members to react to. Sends a bar graph representing the answers as well. Allows a question with up to 4 options.\n*Commands are not case-sensitive*")
 
 bot.run(token)
